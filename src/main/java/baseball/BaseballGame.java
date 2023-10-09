c5f6e0e6f3ce01075e5b69fb5d2a64448cd3fbcf
hlmg <devhlmg@gmail.com>
2023-10-09 11:56:33 +0900

    refactor: 게임 상태에 따라 게임이 진행될 수 있게 수정

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,45 +1,67 @@
 package baseball;
 
+import static baseball.BaseballGame.Status.*;
+
 import java.util.List;
 
 public class BaseballGame {
     private static final int ALL_STRIKE = 3;
     private final InputView inputView;
     private final OutputView outputView;
+    private Status status;
+    Baseballs computer;
 
     public BaseballGame(InputView inputView, OutputView outputView) {
         this.inputView = inputView;
         this.outputView = outputView;
+        status = ENTER;
     }
 
     public void run() {
         outputView.printGameStart();
-        startNewGame();
-    }
-
-    private void startNewGame() {
-        while(true) {
-            List<Integer> generatedRandomNumbers = new RandomNumberGenerator().generateRandomNumbers();
-            Baseballs computer = new Baseballs(generatedRandomNumbers);
-            guessPhase(computer);
-            Command command = Command.from(inputView.getRetryOrEnd());
-            if (command == Command.END) {
-                return;
-            }
+        while (status != EXIT) {
+            play();
         }
     }
 
-    private void guessPhase(Baseballs computer) {
-        while(true) {
-            String playerNumber = inputView.getPlayerNumber();
-            Baseballs player = new Baseballs(playerNumber);
-            int ball = computer.getBall(player);
-            int strike = computer.getStrike(player);
-            outputView.printGameResult(new Hint(ball, strike));
-            if (strike == ALL_STRIKE) {
-                outputView.printGameEnd();
-                return;
-            }
+    private void play() {
+        if (status == ENTER) {
+            generateComputer();
+        } else if (status == GAME_START) {
+            guessComputer();
+        } else if (status == GAME_END) {
+            retryOrExit();
         }
+    }
+
+    private void generateComputer() {
+        List<Integer> generatedRandomNumbers = new RandomNumberGenerator().generateRandomNumbers();
+        computer = new Baseballs(generatedRandomNumbers);
+        status = GAME_START;
+    }
+
+    private void guessComputer() {
+        String playerNumber = inputView.getPlayerNumber();
+        Baseballs player = new Baseballs(playerNumber);
+        int ball = computer.getBall(player);
+        int strike = computer.getStrike(player);
+        outputView.printGameResult(new Hint(ball, strike));
+        if (strike == ALL_STRIKE) {
+            outputView.printGameEnd();
+            status = GAME_END;
+        }
+    }
+
+    private void retryOrExit() {
+        Command command = Command.from(inputView.getRetryOrEnd());
+        if (command == Command.END) {
+            status = EXIT;
+        } else if (command == Command.RETRY) {
+            status = ENTER;
+        }
+    }
+
+    public enum Status {
+        ENTER, GAME_START, GAME_END, EXIT
     }
 }
